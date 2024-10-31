383412b4ab1581821c2bb68002aec47b9f8f079e
fersonaglio <sonaglio3@hotmail.com>
2024-10-31 10:29:16 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express, { request, response } from "express";
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post("/usuarios", (request, response) => {
+  users.push(request.body);
+
+  response.status(201).json(request.body);
+});
+
+app.get("/usuarios", (request, response) => {
+  response.status(200).json(users);
+});
+
+app.listen(3000);
